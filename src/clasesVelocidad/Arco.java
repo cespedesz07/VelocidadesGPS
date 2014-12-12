257601d7ee1281b883a45ea65bc7d8b9ca70ec1d
cespedesz07 <cespedesz07@gmail.com>
2014-12-12 17:02:59 -0500

    Pendiente: Probar la instanciacion de Arcos en el metodo obtenerValoresColumnas

--- src/clasesVelocidad/Arco.java
+++ src/clasesVelocidad/Arco.java
@@ -12,6 +12,7 @@ public class Arco {
 	
 	// Atributos
 	private int idVia;
+	private String layer;
 	private String direccion;
 	private double longitud1;
 	private double latitud1;
@@ -24,8 +25,9 @@ public class Arco {
 	
 	
 	//Métodos
-	public Arco( int idVia, String direccion, double latitud1, double longitud1, double latitud2, double longitud2 ){
+	public Arco( int idVia, String layer, String direccion, double latitud1, double longitud1, double latitud2, double longitud2 ){
 		this.idVia = idVia;
+		this.layer = layer;
 		this.direccion = direccion;
 		this.latitud1 = latitud1;
 		this.longitud1 = longitud1;
@@ -37,8 +39,146 @@ public class Arco {
 	
 	
 	
+	
+	
 	public double calcularVelocidadProm(){
 		return 0.0;
+	}
+
+
+
+
+
+	public int getIdVia() {
+		return idVia;
+	}
+
+
+
+
+
+	public void setIdVia(int idVia) {
+		this.idVia = idVia;
+	}
+
+
+
+
+
+	public String getLayer() {
+		return layer;
+	}
+
+
+
+
+
+	public void setLayer(String layer) {
+		this.layer = layer;
+	}
+
+
+
+
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+
+
+
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+
+
+
+
+	public double getLongitud1() {
+		return longitud1;
+	}
+
+
+
+
+
+	public void setLongitud1(double longitud1) {
+		this.longitud1 = longitud1;
+	}
+
+
+
+
+
+	public double getLatitud1() {
+		return latitud1;
+	}
+
+
+
+
+
+	public void setLatitud1(double latitud1) {
+		this.latitud1 = latitud1;
+	}
+
+
+
+
+
+	public double getLongitud2() {
+		return longitud2;
+	}
+
+
+
+
+
+	public void setLongitud2(double longitud2) {
+		this.longitud2 = longitud2;
+	}
+
+
+
+
+
+	public double getLatitud2() {
+		return latitud2;
+	}
+
+
+
+
+
+	public void setLatitud2(double latitud2) {
+		this.latitud2 = latitud2;
+	}
+
+
+
+
+
+	public ArrayList<Punto> getListaPuntos() {
+		return listaPuntos;
+	}
+
+
+
+
+
+	public void setListaPuntos(ArrayList<Punto> listaPuntos) {
+		this.listaPuntos = listaPuntos;
+	}
+
+
+
+
+
+	public double getVelocidadPromedio() {
+		return velocidadPromedio;
 	}
 	
 
--- src/utilidades/ArchivoRedVial.java
+++ src/utilidades/ArchivoRedVial.java
@@ -12,8 +12,8 @@ import clasesVelocidad.Arco;
 public class ArchivoRedVial {
 	
 	
-	private static final String[] COLUMNAS = { "Layer", 	"longitud", "ID_Via", 	 "Direccion", 
-											   "Longitud1", "Latitud1", "Longitud2", "Latitud2" };		
+	private static final String[] COLUMNAS = { "Layer",     "ID_Via", 	"Direccion", 
+											   "Latitud1", "Longitud1", "Latitud2", "Longitud2" };		
 	private String rutaOrigen;
 	private ArrayList<String[]> contenidoRedVial;	
 	private String delimitador;	
@@ -60,11 +60,27 @@ public class ArchivoRedVial {
 		//Primero se obtiene los indices de las columnas
 		List<String> columnasRedVial = Arrays.asList( this.contenidoRedVial.get(0) );
 		ArrayList<Integer> indicesColumnas = new ArrayList<Integer>();
+		int indiceColumna = 0;
 		for ( String columna : COLUMNAS ){
-			indicesColumnas.in
+			indiceColumna = columnasRedVial.indexOf( columna );
+			indicesColumnas.add( indiceColumna );
 		}
+		
+		int idVia = 0;
+		String layer = "", direccion = "";
+		double latitud1 = 0.0, longitud1 = 0.0, latitud2 = 0.0, longitud2 = 0.0; 
+		//Luego de Tener los indices, se utilizan para consultar las columnas de interes
 		for ( int i=1; i<this.contenidoRedVial.size(); i++ ){
-			
+			String[] fila = this.contenidoRedVial.get(i);
+			layer = fila[ indicesColumnas.get(0) ];
+			idVia = Integer.valueOf( fila[ indicesColumnas.get(1) ] );
+			direccion = fila[ indicesColumnas.get(2) ];
+			latitud1 = Double.parseDouble( fila[ indicesColumnas.get(3) ] );
+			longitud1 = Double.parseDouble( fila[ indicesColumnas.get(4) ] );
+			latitud2 = Double.parseDouble( fila[ indicesColumnas.get(5) ] );
+			longitud2 = Double.parseDouble( fila[ indicesColumnas.get(6) ] );			
+			Arco arco = new Arco( idVia, layer, direccion, latitud1, longitud1, latitud1, longitud1 );
+			this.arregloArcos.add( arco );
 		}
 	}
 
